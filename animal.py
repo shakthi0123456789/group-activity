da5ef863e71b4e9e7ff57f9623797c43edfdc041
shakthi <yasassrishakthi@gmail.com>
2024-02-02 15:15:39 +0530

    python file

---
+++ animal.py
@@ -0,0 +1,13 @@
+import sys
+
+def default():
+    print('Hello')
+
+def main():
+    default()
+
+# This block ensures that the main() function is called only when the script is run directly
+if __name__ == '__main__':
+    main()
+
+
