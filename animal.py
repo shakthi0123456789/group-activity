ff9875b33df8aa8cb582bfad4de94f348ce52472
shakthi <yasassrishakthi@gmail.com>
2024-02-09 14:28:14 +0530

    python file

--- animal.py
+++ animal.py
@@ -1,13 +1,22 @@
 import sys
 
+def cat():
+    print('meow')
+
 def default():
     print('Hello')
 
 def main():
-    default()
+    # Check if there are command-line arguments
+    if len(sys.argv) > 1:
+        if sys.argv[1] == "cat":
+            cat()
+        else:
+            default()
+    else:
+        default()
 
 # This block ensures that the main() function is called only when the script is run directly
 if __name__ == '__main__':
     main()
-
 
