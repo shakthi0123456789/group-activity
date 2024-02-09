eda0c4929c64ca153ddc2b7a5a7a51c1a8fc4630
shakthi <yasassrishakthi@gmail.com>
2024-02-09 14:36:19 +0530

    dog function added

--- animal.py
+++ animal.py
@@ -1,22 +1,17 @@
 import sys
 
-def cat():
-    print('meow')
+def dog():
+    print(‘Baw')
 
 def default():
     print('Hello')
 
 def main():
-    # Check if there are command-line arguments
-    if len(sys.argv) > 1:
-        if sys.argv[1] == "cat":
-            cat()
-        else:
-            default()
+    if sys.argv[1] == ‘dog':
+        dog()
     else:
         default()
 
-# This block ensures that the main() function is called only when the script is run directly
 if __name__ == '__main__':
     main()
 
